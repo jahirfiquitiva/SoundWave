8278b043c294e072f7787bc3ab7d6ad2f9a3a82a
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2017-06-18 20:23:54 -0500

    Arreglar comando de usuarios

--- src/co/edu/uptc/music/persistence/UserSQL.java
+++ src/co/edu/uptc/music/persistence/UserSQL.java
@@ -7,20 +7,13 @@ public class UserSQL {
     }
 
     public String queryUser(String id) {
-        return "SELECT * FROM USER WHERE USER_ID=\'" + id + "\';";
+        return "SELECT * FROM USERS WHERE USER_ID=\'" + id + "\';";
     }
 
     public String insertUser(String type, String id, String name, String email, String userName,
                              String password) {
-       /* return "INSERT INTO USER VALUES(\'" + id + "\', \'" + type + "\', \'" + name + "\', MD5
-       (\'" +
-                password + "\'));";
-*/
-        return "insert into usuario values ('" + id + "','" + type + "','" + name + "','" + email
-                + "','" + userName + "','" + "\', MD5(\'" +
-                password + "\'));";
-
-
+        return "INSERT INTO USERS VALUES(\'" + type + "\', \'" + id + "\', \'" + name + "\', \'"
+                + email + "\', \'" + userName + "\', MD5" + "(\'" + password + "\'));";
     }
 
     public String deleteUser(String id) {
