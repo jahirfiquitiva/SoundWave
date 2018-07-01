49140eafa6df3fd96e3340ac342e2f6a80771d41
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-06-30 20:10:28 -0500

    :fire: Remove unnecesary file

--- web/assets/js/utils.js
+++
@@ -1,6 +0,0 @@
-String.prototype.toCamelCase = function(str) {
-    return str
-        .replace(/\s(.)/g, function($1) { return $1.toUpperCase(); })
-        .replace(/\s/g, '')
-        .replace(/^(.)/, function($1) { return $1.toLowerCase(); });
-}
