1db06da6cc7daccabcb21279287f2ece73f51b4d
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-08-22 13:46:17 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, jsonify
+import socket
+
+app = Flask("SoundWave")
+
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    hoster = socket.gethostbyname(socket.gethostname())
+    app.run(host=hoster)
+    # app.run()
---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+function toggleDrawer() {
+    let check = document.getElementById('drawer-check');
+    if (check !== null && check !== undefined) {
+        check.click();
+    }
+}
