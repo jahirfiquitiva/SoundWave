f18a26188b3d7e244490cb1942fd6a20a4c5ce4e
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-01-31 21:55:59 -0500

    Add default load and get_list methods implementation

--- managers/BaseManager.py
+++ managers/BaseManager.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Generic, TypeVar, Optional
-from repository import BaseSQL as bq, BaseDAO as bdao
+from repository import BaseDAO as bdao
 
 T = TypeVar('T')
 DAO = TypeVar('DAO', bound=bdao.BaseDAO)
@@ -9,6 +9,9 @@ DAO = TypeVar('DAO', bound=bdao.BaseDAO)
 class BaseManager(ABC, Generic[DAO, T]):
     def __init__(self):
         self.items: [T] = []
+
+    def get_items(self) -> [T]:
+        return self.items
 
     def add_item(self, item: T) -> bool:
         if item in self.items:
@@ -28,16 +31,17 @@ class BaseManager(ABC, Generic[DAO, T]):
     def clear_list(self):
         self.items.clear()
 
+    def load(self):
+        self.clear_list()
+        for item in self.dao.query():
+            self.add_item(item)
+
     @abstractmethod
     def find_item(self, id: int) -> Optional[T]:
         raise Exception("You have not implemented this method")
 
     @abstractmethod
     def find_item(self, s: str) -> Optional[T]:
-        raise Exception("You have not implemented this method")
-
-    @abstractmethod
-    def load(self):
         raise Exception("You have not implemented this method")
 
     @property
