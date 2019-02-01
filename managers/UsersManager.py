d9d7fd9bda7d3cbb99a0e8a536bf67ec88cab16b
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-02-01 00:03:59 -0500

    Fix issues with Users Manager

--- managers/UsersManager.py
+++ managers/UsersManager.py
@@ -1,46 +1,29 @@
 from typing import Optional
-from models import User as u
+from models import User as us
 from repository.user import UserDAO as udao
 from managers import BaseManager as bm
-from managers.BaseManager import DAO, T
 
 
 class UsersManager(bm.BaseManager):
     def __init__(self):
         super().__init__()
 
-    def add_new_user(self, name: str, last_name: str, age: int, nick: str,
-                     email: str, password: str) -> bool:
-        return self.dao.insert(
+    def create(self, name: str, last_name: str, age: int, nick: str,
+               email: str, password: str) -> bool:
+        created = self.dao.insert(
             self.dao.get_insert_query(
-                u.User(0, name, last_name, age, nick, email, password)))
+                us.User(0, name, last_name, age, nick, email, password)))
+        if created:
+            self.load()
+            return True
+        return False
 
-    def find_item(self, user_id: int) -> Optional[T]:
-        for item in self.items:
-            if item.id == user_id:
-                return item
-        return None
-
-    def find_item(self, s: str) -> Optional[T]:
-        for item in self.items:
-            if item.nick.lower() == s.lower():
-                return item
-        return None
-
-    def get_users(self) -> []:
-        return self.items
-
-    def load(self):
-        self.clear_list()
-        for item in self.dao.query():
-            self.add_item(item)
-
-    def modify_user(self, user_id: int, name: str, last_name: str, age: int,
-                    nick: str, email: str, password: str):
-        self.dao.update_executor(
-            "update %s set(name_%s='%s', last_%s='%s', "
-            "age_%s='%d', nick_%s='%s', email_%s='%s', "
-            "password_%s='%s' ) where id_%s= '%d';" %
+    def update(self, user_id: int, name: str, last_name: str, age: int,
+               nick: str, email: str, password: str) -> bool:
+        updated = self.dao.update_executor(
+            "update %s set name_%s='%s', last_name_%s='%s', "
+            "age_%s=%d, nick_%s='%s', email_%s='%s', "
+            "password_%s=md5('%s') where id_%s=%d;" %
             (self.dao.sql.table_name,
              self.dao.sql.table_name, name,
              self.dao.sql.table_name, last_name,
@@ -49,13 +32,38 @@ class UsersManager(bm.BaseManager):
              self.dao.sql.table_name, email,
              self.dao.sql.table_name, password,
              self.dao.sql.table_name, user_id))
+        if updated:
+            self.load()
+            return True
+        return False
 
-    def delete_user(self, user_id: int) -> bool:
-        user = self.find_item(user_id)
-        if user is None:
-            return False
-        return self.remove_item(user)
+    def delete(self, user_id: int) -> bool:
+        if self.dao.delete(user_id):
+            self.load()
+            return True
+        return False
+
+    # noinspection PyBroadException
+    def tuple_to_item(self, tuple_ref: tuple) -> Optional[us.User]:
+        try:
+            return us.User(tuple_ref[0], tuple_ref[1], tuple_ref[2], tuple_ref[3], tuple_ref[4],
+                           tuple_ref[6], tuple_ref[5])
+        except Exception:
+            pass
+        return None
+
+    def find_item_by_id(self, user_id: int) -> Optional[us.User]:
+        for item in self.get_items():
+            if item.id == user_id:
+                return item
+        return None
+
+    def find_item(self, s: str) -> Optional[us.User]:
+        for item in self.get_items():
+            if item.nick.lower() == s.lower():
+                return item
+        return None
 
     @property
-    def dao(self) -> DAO:
+    def dao(self) -> udao.UserDAO:
         return udao.UserDAO()
