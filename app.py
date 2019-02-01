9f6fbf601deb15d49f8018480df82e1d4da7e0e9
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-02-01 00:06:37 -0500

    Update sample code

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 import socket
-from repository.DatabaseConnection import DatabaseConnection as dabacon
-from repository.user import UserDAO as udao
+from managers import UsersManager as um
 
 app = Flask("SoundWave")
 
@@ -12,10 +11,15 @@ def main():
 
 
 if __name__ == '__main__':
-    print("Hello")
-    dao = udao.UserDAO()
-    for i in dao.query():
-        print(str(i))
+    userManager = um.UsersManager()
+    # userManager.create("TestName", "TestSurname", 1, "tester", "tester@soundwave.co", "1234")
+    # userManager.update(6, 'actualizado', 'otra_cosa', 34, 'lizado', 'freger@gmail.co', '4321')
+    items = userManager.get_items()
+    if items is not None and len(items) > 0:
+        for i in items:
+            print(str(i))
+    else:
+        print("No users found")
     # app.config['TEMPLATES_AUTO_RELOAD'] = True
     # hoster = socket.gethostbyname(socket.gethostname())
     # app.run(host=hoster)
