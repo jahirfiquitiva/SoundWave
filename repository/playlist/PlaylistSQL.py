dfeb6a74428d5dd24fb342384e794c095581e5c8
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-02-02 22:53:35 -0500

    Fix incorrect query for playlists

--- repository/playlist/PlaylistSQL.py
+++ repository/playlist/PlaylistSQL.py
@@ -6,7 +6,7 @@ class PlaylistSQL(bq.BaseSQL):
         return "playlist"
 
     def insert_playlist(self, name: str, user_id: int) -> str:
-        return "insert into %s (name_%s,user_id_%s) values (%s, %s);" % (
+        return "insert into %s (name_%s,user_id_%s) values ('%s', '%s');" % (
             self.table_name, self.table_name, self.table_name, name, user_id)
 
     def query_by_user_id(self, user_id: int) -> str:
