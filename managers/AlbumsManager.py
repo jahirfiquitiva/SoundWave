9597fdca59ef6a2eb6b7330f0150e5a87d2cfdcf
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-02-03 00:52:55 -0500

    Fix issues with album queries

--- managers/AlbumsManager.py
+++ managers/AlbumsManager.py
@@ -1,51 +1,62 @@
 from typing import Optional
-from models import Album as album
-from repository.album import AlbumDAO as albumDao
+from models import Album as al
+from repository.album import AlbumDAO as aldao
 from managers import BaseManager as bm
-from managers.BaseManager import DAO, T
 
 
 class AlbumsManager(bm.BaseManager):
-
-    def find_item(self, user_id: int) -> Optional[T]:
-        for item in self.items:
-            if item.id == user_id:
-                return item
-        return None
-
-    def load(self):
-        self.clear_list()
-        for item in self.dao.query():
-            self.add_item(item)
-
     def __init__(self):
         super().__init__()
 
-    def get_albums(self) -> Optional[T]:
-        return self.items
+    def create(self, name: str, img_path: str, release_year: int, artist_id: int) -> bool:
+        created = self.dao.insert(
+            self.dao.get_insert_query(al.Album(0, name, img_path, release_year, artist_id),
+                                      artist_id))
+        if created:
+            self.load()
+            return True
+        return False
 
-    def add_album(self,
-                  name: str, img_path: str, release_year: int,
-                  artist_id: int):
-        self.dao.insert(album.Album(0, name, img_path, release_year))
+    def update(self, album_id: int, name: str, img_path: str, release_year: int,
+               artist_id: int) -> bool:
+        updated = self.dao.update_executor(
+            "update %s set name_%s='%s', img_path_%s='%s', release_year_%s='%d', "
+            "artist_id_artist='%d' where id_%s='%d'" %
+            (self.dao.sql.table_name,
+             self.dao.sql.table_name, name, self.dao.sql.table_name, img_path,
+             self.dao.sql.table_name, release_year, album_id,
+             self.dao.sql.table_name, artist_id))
+        if updated:
+            self.load()
+            return True
+        return False
 
-    def delete_album(self, album_id: int):
-        self.load()
-        al = self.find_item(album_id)
-        if al is None:
-            return False
-        return self.remove_item(al)
+    def delete(self, album_id: int) -> bool:
+        if self.dao.delete(album_id):
+            self.load()
+            return True
+        return False
 
-    def modify_album(self, album_id: int, name: str, img_path: str, release_year: int,
-                     artist_id: int):
-        self.dao.update_executor(
-            "update %s set("
-            "name_album_%s='%s',"
-            "img_path_album_%s='%s',"
-            "release_year_album_%s='%d',"
-            "artist_id_artist_%s='%d') where id_album_%s='%d'" %
-            (self.dao.sql.table_name, self.dao.sql.table_name, name, self.dao.sql.table_name, img_path, self.dao.sql.table_name,
-             release_year, self.dao.sql.table_name, album_id, self.dao.sql.table_name, artist_id))
+    # noinspection PyBroadException
+    def tuple_to_item(self, tuple_ref: tuple) -> Optional[al.Album]:
+        try:
+            return al.Album(tuple_ref[0], tuple_ref[1], tuple_ref[2], tuple_ref[3], tuple_ref[4])
+        except Exception:
+            pass
+        return None
 
-    def dao(self) -> DAO:
-        return albumDao.AlbumDAO()
+    def find_item_by_id(self, album_id: int) -> Optional[al.Album]:
+        for item in self.get_items():
+            if item.id == album_id:
+                return item
+        return None
+
+    def find_item(self, s: str) -> Optional[al.Album]:
+        for item in self.get_items():
+            if item.name.lower() == s.lower():
+                return item
+        return None
+
+    @property
+    def dao(self) -> aldao.AlbumDAO:
+        return aldao.AlbumDAO()
--- repository/album/AlbumSQL.py
+++ repository/album/AlbumSQL.py
@@ -8,7 +8,7 @@ class AlbumSQL(bq.BaseSQL):
 
     def insert_album(self, name: str, img_path_album: str, release_year_album: int,
                      artist_id_artist: int) -> str:
-        return "insert into %s (name_%s, img_path_album_%s, release_year_album_%s, " \
-               "artist_id_artist_%s) values('%s', '%s', '%s', '%s');" % (
+        return "insert into %s (name_%s, img_path_%s, release_year_%s, " \
+               "artist_id_artist) values('%s', '%s', %d, %d);" % (
                    self.table_name, self.table_name, self.table_name, self.table_name,
-                   self.table_name, name, img_path_album, release_year_album, artist_id_artist)
+                   name, img_path_album, release_year_album, artist_id_artist)
