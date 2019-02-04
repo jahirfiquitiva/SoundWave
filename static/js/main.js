2afe236765e447474967cc189df401b83f19f0be
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-02-03 19:42:05 -0500

    Redesign main site. Improve look of drawer

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,8 @@
 function toggleDrawer() {
+    let drawer = document.getElementById('drawer');
+    if (drawer) {
+        drawer.classList.remove('is-hidden');
+    }
     let check = document.getElementById('drawer-check');
     if (check !== null && check !== undefined) {
         check.click();
