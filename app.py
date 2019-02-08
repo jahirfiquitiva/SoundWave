556842bae23924dc6e664745b5f6fab60dc042d8
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-02-07 22:57:57 -0500

    Add initial login site implementation

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import socket
 
 app = Flask("SoundWave")
 
@@ -9,7 +8,14 @@ def main():
     return render_template('index.html')
 
 
+@app.route("/login")
+@app.route("/login.html")
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
+    # Flask WebApp
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     # app.run(host=socket.gethostbyname(socket.gethostname()))
     app.run()
